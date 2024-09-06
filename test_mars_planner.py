43275f61792d2b2e18cc9683a871f15bfee26d9e
Christopher Brooks <cbrooks@usfca.edu>
2024-09-05 20:15:41 -0700

    Initial commit

---
+++ test_mars_planner.py
@@ -0,0 +1,27 @@
+from unittest import TestCase
+from mars_planner import *
+
+
+class TestRoverState(TestCase):
+    pass
+
+
+class Test(TestCase):
+    def test_move_to_sample(self):
+        s = RoverState(loc="battery")
+        move_to_sample(s)
+        self.assertEqual(s.loc, "sample")
+
+    def test_eq(self):
+        s = RoverState(loc="battery")
+        s2 = RoverState(loc="battery")
+        self.assertEqual(s,s2)
+        s3 = RoverState(loc="station")
+        self.assertNotEqual(s, s3)
+
+
+class TestRoverState(TestCase):
+    def test_successors(self):
+        s=RoverState()
+        slist = s.successors(action_list)
+        print(slist)
